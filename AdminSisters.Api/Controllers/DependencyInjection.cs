061e64eeec4632da48d80e92b395d2229119fb0b
wronah <hubertwro7@gmail.com>
2025-06-05 14:51:10 +0200

    add MediatR

---
+++ AdminSisters.Api/Controllers/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace AdminSisters.Api.Controllers;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
--- AdminSisters.Api/Program.cs
+++ AdminSisters.Api/Program.cs
@@ -1,6 +1,9 @@
+using AdminSisters.Api.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDependencyInjection();
 
 builder.Services.AddControllers();
 
