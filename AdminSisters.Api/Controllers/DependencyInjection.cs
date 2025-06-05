b4af380e230d854de991a0de1be78f39efd463d6
wronah <hubertwro7@gmail.com>
2025-06-05 14:53:40 +0200

    add cors policy

--- AdminSisters.Api/Controllers/DependencyInjection.cs
+++ AdminSisters.Api/Controllers/DependencyInjection.cs
@@ -7,6 +7,16 @@ public static class DependencyInjection
     public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: "AllowAllOrigins",
+                configurePolicy: policy =>
+                {
+                    policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
 
         return services;
     }
