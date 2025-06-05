41e8ea0a7bedd5255d94c9edecdfc453be810877
wronah <hubertwro7@gmail.com>
2025-06-06 01:27:25 +0200

    add address to location dto

--- UseCases/Locations/Dtos/LocationDto.cs
+++ UseCases/Locations/Dtos/LocationDto.cs
@@ -6,4 +6,5 @@ public class LocationDto(Location location)
 {
     public Guid Id { get; init; } = location.Id;
     public string Name { get; init; } = location.Name;
+    public string Address { get; init; } = location.Address;
 }
