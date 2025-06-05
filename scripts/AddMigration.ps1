f169a85c12ef0caec40fa5923efe31b1a01fd6a6
Dominik Podraza <onimod05@gmail.com>
2025-06-05 20:49:47 +0200

    Init

--- scripts/AddMigration.ps1
+++
@@ -1,5 +0,0 @@
-$path = $MyInvocation.MyCommand.Path
-$parentPath = $(Split-Path (Split-Path $path -Parent) -Parent)
-$message = $(Read-Host -Prompt 'Input message')
-
-dotnet ef migrations add $message --project $parentPath\src\AdminSisters.Api --startup-project $parentPath\src\AdminSisters.Api --output-dir $parentPath\src\AdminSisters.Api\Persistence\Migrations --context MainDbContext
--- scripts/UpdateDatabase.ps1
+++
@@ -1,4 +0,0 @@
-$path = $MyInvocation.MyCommand.Path
-$parentPath = $(Split-Path (Split-Path $path -Parent) -Parent)
-
-dotnet ef database update --project $parentPath\src\AdminSisters.Api --startup-project $parentPath\src\AdminSisters.Api --context MainDbContext
