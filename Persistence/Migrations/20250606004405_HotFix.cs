645e1c997c43e932512154fecac61fba27c03c87
Dominik Podraza <onimod05@gmail.com>
2025-06-06 02:44:39 +0200

    add initial migration for HotFix with empty Up and Down methods

---
+++ Persistence/Migrations/20250606004405_HotFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace be_adminsisters.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
