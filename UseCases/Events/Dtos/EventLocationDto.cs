f680727cb14fd9c56a9a887ff32a5cae4abbc2ed
Dominik Podraza <onimod05@gmail.com>
2025-06-06 04:04:26 +0200

    add Longitude and Latitude properties to EventLocationDto

--- UseCases/Events/Dtos/EventLocationDto.cs
+++ UseCases/Events/Dtos/EventLocationDto.cs
@@ -7,4 +7,6 @@ public class EventLocationDto(Location location)
     public Guid Id { get; set; } = location.Id;
     public string Name { get; set; } = location.Name;
     public string Address { get; set; } = location.Address;
+    public double Longitude { get; set; } = location.Longitude;
+    public double Latitude { get; set; } = location.Latitude;
 }
