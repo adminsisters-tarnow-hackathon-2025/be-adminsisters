12b491dbb5352fc93595a70f0a0fed98686875f6
Dominik Podraza <onimod05@gmail.com>
2025-06-06 04:09:21 +0200

    add Type property to EventDto

--- UseCases/Events/Dtos/EventDto.cs
+++ UseCases/Events/Dtos/EventDto.cs
@@ -12,5 +12,6 @@ public class EventDto(Event eventEntity)
     public int CoinReward { get; set; } = eventEntity.CoinReward;
     public DateTime DateFrom { get; set; } = eventEntity.DateFrom;
     public DateTime DateTo { get; set; } = eventEntity.DateTo;
+    public string Type { get; set; } = eventEntity.Type;
     public EventLocationDto Location { get; set; } = new EventLocationDto(eventEntity.Location!);
 }
